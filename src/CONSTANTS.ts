04aacb86bd118147293b98dd473ba311f1c90048
Giorgi Nefaridze <nefogio1@gmail.com>
2023-09-24 18:12:22 +0400

    delete car controller by number plate

--- src/CONSTANTS.ts
+++ src/CONSTANTS.ts
@@ -1,15 +1,18 @@
 export const errorMessages = {
   invaliCredentials:
-    "Please enter a valid credentials(it's too short or incorrect data).",
+    "Please enter a valid credentials (it's too short or incorrect data).",
   userExists: "User with this email already exists",
   internalError:
     "Error may be caused by invalid input format or database is down",
   missingToken:
     "Please make sure that you are logged in or have valid access token(Http headers)",
+  noPermission:
+    "Sorry, you are not owner of this car (check number plate again)",
 };
 
 export const successMessages = {
   userCreated: "User created successfully 🚀",
   passwordChanged: "Password has been changed 🤫",
   carCreated: "Car has been created 🏎️",
+  carRemoved: "Car has been removed successfully",
 };
